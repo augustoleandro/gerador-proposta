7aac420488494b310814d65b6b5c3613a067f596
Leandro Augusto <augustoleandro@gmail.com>
2024-10-07 16:50:14 -0300

    implementing city select

--- app/(main)/(proposals)/components/OrderInput.tsx
+++ app/(main)/(proposals)/components/OrderInput.tsx
@@ -1,4 +1,13 @@
+"use client";
+
 import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Order } from "@/lib/types";
 import { useState } from "react";
 import { NewOrderDialog } from "./NewOrderDialog";
@@ -8,17 +17,40 @@ interface OrderInputProps {
   existingOrders: Order[];
 }
 
+const city_options = {
+  GYN: "Goiânia",
+  BSB: "Brasília",
+};
+
 export function OrderInput({ onOrderAdded, existingOrders }: OrderInputProps) {
   const [order_number, setOrderNumber] = useState<string>("");
-
+  const [city, setCity] = useState<string>("GYN");
   const handleAddOrder = (newOrder: Order) => {
     onOrderAdded(newOrder);
     setOrderNumber("");
   };
 
   return (
-    <div className="flex-col w-full">
+    <div className="flex-col w-full mt-2">
       <div className="flex gap-2">
+        <Select
+          defaultValue={city}
+          onValueChange={(value) => {
+            setCity(value);
+          }}
+          value={city}
+        >
+          <SelectTrigger>
+            <SelectValue placeholder="Selecione a cidade" />
+          </SelectTrigger>
+          <SelectContent>
+            {Object.entries(city_options).map(([key, value]) => (
+              <SelectItem key={key} value={key}>
+                {value}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
         <Input
           placeholder="Nº do pedido"
           value={order_number}
